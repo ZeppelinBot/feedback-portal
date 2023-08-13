68429199bc5db7cd830245bde1e1bd2f9f7904c8
Dragory <2606411+Dragory@users.noreply.github.com>
2023-08-13 16:29:56 +0000

    feat: custom esm migration generator

--- mikro-orm.config.js
+++ mikro-orm.config.js
@@ -1,3 +1,30 @@
+// NOTE: This config file is only used for the mikro-orm CLI
+// For app DB config, see src/orm.ts
+
+import { JSMigrationGenerator } from "@mikro-orm/migrations";
+
+class EsmMigrationGenerator extends JSMigrationGenerator {
+  generateMigrationFile(className, diff) {
+    return `
+"use strict";
+
+import { Migration } from '@mikro-orm/migrations';
+
+export class ${className} extends Migration {
+
+  async up() {
+    ${diff.up.map(sql => this.createStatement(sql, 4)).join("\n").trim()}
+  }
+
+  async down() {
+    ${diff.down.map(sql => this.createStatement(sql, 4)).join("\n").trim()}
+  }
+
+}
+    `.trim();
+  }
+}
+
 export default {
   entities: [],
 
@@ -11,6 +38,7 @@ export default {
     path: "./migrations",
     snapshot: false,
     emit: "js",
+    generator: EsmMigrationGenerator,
   },
 
   discovery: {
